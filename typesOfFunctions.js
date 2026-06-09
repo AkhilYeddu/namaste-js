c19c86a114fd8e5d5ff2c36d7abec824344554c7
AkhilYeddu <akhilkiu10@gmail.com>
2026-06-10 04:06:32 +0530

    typesofFN- Anonymous, NME, etc

--- typesOfFunctions.js
+++ typesOfFunctions.js
@@ -9,3 +9,29 @@ var b = function(){
     console.log("B called!");
 }
 b(); // function expressions are not hoisted
+
+// Anonymous Function
+
+function(){
+    console.log("hi")
+}
+
+// Named Function Expression
+
+var b = function xyz(){
+    console.log("hello");
+    xyz(); // possible
+
+ 
+}
+b();
+// xyz();  not possible
+
+
+// Arguements V/S Parameters
+
+function example(Parameters){
+    console.log("Hello");
+}
+
+example(Arguements)
