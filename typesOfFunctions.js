3b44f3a881dd32b8573470d589f442070608a93a
AkhilYeddu <akhilkiu10@gmail.com>
2026-06-10 03:53:41 +0530

    update fn statement and expression

--- typesOfFunctions.js
+++ typesOfFunctions.js
@@ -1,1 +1,11 @@
 // Function Statement
+function a(){
+    console.log("A called!");
+}
+a();
+
+// Function Expression
+var b = function(){
+    console.log("B called!");
+}
+b(); // function expressions are not hoisted
