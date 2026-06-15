1536a9c6e5b714eb4adde70533c5b39a4b17cd2c
AkhilYeddu <akhilkiu10@gmail.com>
2026-06-16 01:07:13 +0530

    added HOF-functions

---
+++ hof.js
@@ -0,0 +1,22 @@
+const circles = [3, 1, 2, 4]
+
+const area = function(radius){          // callback function
+    return Math.PI * radius * radius 
+}
+const circumference = function(radius){         // callback function
+    return 2* Math.PI * radius
+}
+const diameter = function(radius){          // callback function
+    return 2 * radius
+}
+
+Array.prototype.calculate = function(logic){
+    
+    const output = []
+    for(let i = 0 ; i < this.length; i++){ // here, calcuate is a HIGHER ORDER FUNCTION as it is taking other functions as arguements
+        output.push(logic(this[i]))         // polyphyl of MAP function
+    }
+    return output;
+}
+
+console.log(circles.calculate(area))
