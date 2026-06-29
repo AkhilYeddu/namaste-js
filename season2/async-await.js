8616e7dc86dd9082dc95bbde9db18a4f734a0152
AkhilYeddu <akhilkiu10@gmail.com>
2026-06-29 19:58:13 +0530

    ex2 : async await

--- season2/async-await.js
+++ season2/async-await.js
@@ -1,6 +1,6 @@
 // async - keyword that is used before a function to create an async function
 
-const p = new Promise(function(resolve, reject){
+const p1 = new Promise(function(resolve, reject){
     setTimeout(function(){
         resolve("promise fullfilled!")
     },5000)
@@ -8,13 +8,21 @@ const p = new Promise(function(resolve, reject){
     
 });
 
+const p2 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve("promise fullfilled!")
+    },2000)
+    
+    
+});
+
 async function handlePromise(){   // here js engine waits till the promise is resolved
     console.log("print me first!")
-    const val1 = await p;
+    const val1 = await p1;
     console.log("checking1")
     console.log(val1)
-                                    // both gets printed simultaneously !!
-    const val2 = await p;
+                    // waits for 10 secs at p1, then the p2 gets printed simultaneously
+    const val2 = await p2;
     console.log("checking2")
     console.log(val2)
     
