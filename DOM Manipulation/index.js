bc6951f8669f2b303f5eb18ea0837a9f91db1266
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 02:56:02 +0530

    removing element attributes

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -3,11 +3,7 @@ const div = document.querySelector("div")
 const span1 = document.querySelector("#hi")
 const span2 = document.getElementById("bye")
 
-console.log(span1.getAttribute("title"))
-console.log(span1.title)
-
-console.log(span1.setAttribute("id","super id"))
-span1.id = "awesome id"
+span1.removeAttribute("title")
 
 
 
