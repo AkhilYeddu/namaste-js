55c423a4a8648a83084f08034a3a54143845b9f3
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 02:43:05 +0530

    safer way instead of inner HTML

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -1,17 +1,11 @@
-// Adding element
 const body = document.body
-// body.append("HELLO WORLD!")
-
-// creating element
 const div = document.createElement("div")
+const strong = document.createElement("strong")
+
+strong.textContent = "Hello im Strong!"
+
+div.append(strong)
 body.append(div)
 
-// modifying element text
-// div.textContent ="Hello World! "
 
-// const div = document.querySelector("div")
-// console.log(div.innerText)
-// console.log(div.textContent)
 
-// if you want to render HTML inside a div, or any other HTML element, we do it using .innerHTML()
-div.innerHTML ="<strong> Hello World!</strong>"
