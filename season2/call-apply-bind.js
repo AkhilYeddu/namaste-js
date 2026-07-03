a07b8a72d046fe403cce8777c731b4701dbd8947
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 01:32:33 +0530

    seperate the  method to reuse

--- season2/call-apply-bind.js
+++ season2/call-apply-bind.js
@@ -1,15 +1,18 @@
 // how does call method work? -> used for function/method borrowing frpm other objects
+const printName = function(){
+        console.log(this.firstName +  " " + this.lastName)
+    }
+
 const name = {
     firstName : "Akhil",
     lastName : "Yeddu",
-    printName : function(){
-        console.log(this.firstName +  " " + this.lastName)
-    }
+   
 }
-name.printName();
+printName.call(name);
+
 
 const name2 = {
     firstName : "Nikhil",
     lastName : "Yeddu"
 }
-name.printName.call(name2);
+printName.call(name2);
