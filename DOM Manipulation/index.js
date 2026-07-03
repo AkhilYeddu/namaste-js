0032bff7e41a4c4764a32f0ca2591cd2874d7685
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 02:29:47 +0530

    modifying text

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -1,7 +1,12 @@
 // Adding element
-const body = document.body
-body.append("HELLO WORLD!")
+// const body = document.body
+// body.append("HELLO WORLD!")
 
 // creating element
-const div = document.createElement("div")
-body.append(div)
+// const div = document.createElement("div")
+// body.append(div)
+
+// modifying element text
+div.innerText = "Hello Bro"
+
+const div = document.querySelector("div")
