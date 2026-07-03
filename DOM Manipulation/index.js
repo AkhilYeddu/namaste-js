2316c5c498dccfd5dbd28d008665cbf2d649e5fc
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 03:10:16 +0530

    modifying element style

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -3,13 +3,9 @@ const div = document.querySelector("div")
 const span1 = document.querySelector("#hi")
 const span2 = document.getElementById("bye")
 
-span1.classList.add("new")
-span1.classList.remove("hi1")
-span1.classList.toggle("hi1") // removes if exists, adds if not present
-span1.classList.toggle("hi1",true) // adding
-span1.classList.toggle('hi1', false) // removing
-
-
+// style.(proerty name)
+span1.style.color = "red"
+span1.style.backgroundColor = "cyan";
 
 
 
