9975ce6b399e7d74005e7390546212e04d8be1cf
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 03:01:46 +0530

    modifying data attributes

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -3,7 +3,9 @@ const div = document.querySelector("div")
 const span1 = document.querySelector("#hi")
 const span2 = document.getElementById("bye")
 
-span1.removeAttribute("title")
+console.log(span1.dataset.longerName) // gives all the data attributes associated with this element
+span1.dataset.addName = "adding a data attribute"
+
 
 
 
