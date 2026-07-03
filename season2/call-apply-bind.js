dac4e535da136ffe13390db98de1dac3fcb5d918
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 01:36:07 +0530

    sending arguments

--- season2/call-apply-bind.js
+++ season2/call-apply-bind.js
@@ -1,18 +1,17 @@
 // how does call method work? -> used for function/method borrowing frpm other objects
-const printName = function(){
-        console.log(this.firstName +  " " + this.lastName)
-    }
-
+const printName = function(hometown, state){
+        console.log(this.firstName +  " " + this.lastName + " from " + hometown + ", " + state)
+}
 const name = {
     firstName : "Akhil",
     lastName : "Yeddu",
    
 }
-printName.call(name);
+printName.call(name, "Vizag", "Andhra Pradesh");
 
 
 const name2 = {
     firstName : "Nikhil",
     lastName : "Yeddu"
 }
-printName.call(name2);
+printName.call(name2, "Hyderabad", "Telangana");
