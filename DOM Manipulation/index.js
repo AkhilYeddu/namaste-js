1d6b91cbfff94f7cfaba80ff852da36dce1ca6a5
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 02:40:14 +0530

    DOMM : innerHTML

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -1,14 +1,17 @@
 // Adding element
-// const body = document.body
+const body = document.body
 // body.append("HELLO WORLD!")
 
 // creating element
-// const div = document.createElement("div")
-// body.append(div)
+const div = document.createElement("div")
+body.append(div)
 
 // modifying element text
-// div.innerText = "Hello Bro"
+// div.textContent ="Hello World! "
 
-const div = document.querySelector("div")
-console.log(div.innerText)
-console.log(div.textContent)
+// const div = document.querySelector("div")
+// console.log(div.innerText)
+// console.log(div.textContent)
+
+// if you want to render HTML inside a div, or any other HTML element, we do it using .innerHTML()
+div.innerHTML ="<strong> Hello World!</strong>"
