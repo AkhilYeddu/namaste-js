96c12306e4b6ab9b4f5b6efc8e9fb64470aaae1a
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-04 03:07:14 +0530

    modifying element classes

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -3,9 +3,11 @@ const div = document.querySelector("div")
 const span1 = document.querySelector("#hi")
 const span2 = document.getElementById("bye")
 
-console.log(span1.dataset.longerName) // gives all the data attributes associated with this element
-span1.dataset.addName = "adding a data attribute"
-
+span1.classList.add("new")
+span1.classList.remove("hi1")
+span1.classList.toggle("hi1") // removes if exists, adds if not present
+span1.classList.toggle("hi1",true) // adding
+span1.classList.toggle('hi1', false) // removing
 
 
 
